f1f4e09ff6e936f3e4c78dc29572475ac1d1b50e
Wojciech Rybakiewicz <98404816+wrybakiewicz@users.noreply.github.com>
2023-08-10 17:15:12 +0200

    feat: add arbitrum data service examples (#3)

---
+++ test/ArbitrumDemoExample.js
@@ -0,0 +1,34 @@
+const { formatBytes32String } = require("ethers/lib/utils");
+const { WrapperBuilder } = require("@redstone-finance/evm-connector");
+
+describe("ArbitrumDemoExample", function () {
+  let contract;
+
+  beforeEach(async () => {
+    // Deploy contract
+    const ArbitrumDemoExample = await ethers.getContractFactory("ArbitrumDemoExample");
+    contract = await ArbitrumDemoExample.deploy();
+  });
+
+  it("Get ARB price securely", async function () {
+    // Wrapping the contract
+    const wrappedContract = WrapperBuilder.wrap(contract).usingDataService({
+      dataFeeds: ["ARB"],
+    });
+
+    // Interact with the contract (getting oracle value securely)
+    const vstPriceFromContract = await wrappedContract.getLatestPrice(formatBytes32String("ARB"));
+    console.log({ vstPriceFromContract });
+  });
+
+  it("Get GMX price securely", async function () {
+    // Wrapping the contract
+    const wrappedContract = WrapperBuilder.wrap(contract).usingDataService({
+      dataFeeds: ["GMX"],
+    });
+
+    // Interact with the contract (getting oracle value securely)
+    const swethPriceFromContract = await wrappedContract.getLatestPrice(formatBytes32String("GMX"));
+    console.log({ swethPriceFromContract });
+  });
+});
---
+++ test/ArbitrumProdExample.js
@@ -0,0 +1,34 @@
+const { formatBytes32String } = require("ethers/lib/utils");
+const { WrapperBuilder } = require("@redstone-finance/evm-connector");
+
+describe("ArbitrumProdExample", function () {
+  let contract;
+
+  beforeEach(async () => {
+    // Deploy contract
+    const ArbitrumProdExample = await ethers.getContractFactory("ArbitrumProdExample");
+    contract = await ArbitrumProdExample.deploy();
+  });
+
+  it("Get ARB price securely", async function () {
+    // Wrapping the contract
+    const wrappedContract = WrapperBuilder.wrap(contract).usingDataService({
+      dataFeeds: ["ARB"],
+    });
+
+    // Interact with the contract (getting oracle value securely)
+    const vstPriceFromContract = await wrappedContract.getLatestPrice(formatBytes32String("ARB"));
+    console.log({ vstPriceFromContract });
+  });
+
+  it("Get GMX price securely", async function () {
+    // Wrapping the contract
+    const wrappedContract = WrapperBuilder.wrap(contract).usingDataService({
+      dataFeeds: ["GMX"],
+    });
+
+    // Interact with the contract (getting oracle value securely)
+    const swethPriceFromContract = await wrappedContract.getLatestPrice(formatBytes32String("GMX"));
+    console.log({ swethPriceFromContract });
+  });
+});
