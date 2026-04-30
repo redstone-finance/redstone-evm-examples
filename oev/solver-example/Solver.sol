7dd587956bc857b17fc3e547e8ff094186ca8928
ppedziwiatr <ppedziwiatr@gmail.com>
2026-04-30 15:23:14 +0200

    fix

--- oev/solver-example/Solver.sol
+++ oev/solver-example/Solver.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/IMorpho.sol";
+
 interface ISwapRouter {
     struct ExactInputSingleParams {
         address tokenIn;
